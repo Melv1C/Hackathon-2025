72c34c3488dde0ace9c04ca555204c93695baf57
bistouri <auguste.elengesa@gmail.com>
2025-03-29 22:18:44 +0100

    Ajout d'une fonction pour envoyer des mails

---
+++ backend/utils/email.py
@@ -0,0 +1,24 @@
+import smtplib
+
+from email.message import EmailMessage
+import ssl
+
+def send(message:str, adress, subject = ""):
+    # Create a text/plain message
+    msg = EmailMessage()
+    msg.set_content(message)
+    senderemail = "capsuletemporelle2025@gmail.com"
+
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = subject
+    msg['From'] = senderemail
+    msg['To'] = adress
+
+    port = 465
+    password = "ncfp ahry kumi xiwv" #Faudrais pas mettre le mdp mais vsy
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(senderemail, password)
+        server.sendmail(senderemail, adress, msg.as_string())
+
