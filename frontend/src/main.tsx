bb3c2853473a26cb838749fa6d76200d0597481a
Melv1C <claes.melvyn@gmail.com>
2025-03-30 00:31:33 +0100

    feat: add date picker support using MUI x-date-pickers and integrate into CreateCapsulePage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -23,10 +25,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <ThemeProvider theme={theme}>
-                <CssBaseline /> {/* Normalize CSS */}
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <CssBaseline /> {/* Normalize CSS */}
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </LocalizationProvider>
             </ThemeProvider>
         </QueryClientProvider>
     </React.StrictMode>
